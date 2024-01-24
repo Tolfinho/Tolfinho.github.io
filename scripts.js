51f3c3052be02133b3bdc9153575384cac8d2244
João Tolfo <98781282+Tolfinho@users.noreply.github.com>
2024-01-23 21:19:09 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,410 +1,333 @@
-console.log('Hello World from Tetris!!!!!!');
-
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 420;
-canvas.height = 600;
+canvas.width = 500;
+canvas.height = 500;
 
-// Classes
 class InputHandler {
   constructor(game) {
     this.game = game;
 
     document.addEventListener('keydown', (e) => {
-      console.log(e.key);
       if (
-        (e.key === 'ArrowLeft' || e.key === 'a') &&
-        this.game.isFree('left')
-      ) {
-        this.game.currentPiece.x -= this.game.squareWidth;
-      } else if (
-        (e.key === 'ArrowRight' || e.key === 'd') &&
-        this.game.isFree('right')
-      ) {
-        this.game.currentPiece.x += this.game.squareWidth;
-      } else if (
-        (e.key === 'ArrowDown' || e.key === 's') &&
-        this.game.isFree('down')
-      ) {
-        this.game.currentPiece.y += this.game.squareHeight;
-      } else if (e.key === 'ArrowUp' || e.key === 'w') {
-        this.game.currentPiece.matrixToRight();
-      }
+        e.key === 'ArrowRight' &&
+        (this.game.curDir !== 'left' || this.game.parts.length === 0)
+      )
+        this.game.curDir = 'right';
+      else if (
+        e.key === 'ArrowLeft' &&
+        (this.game.curDir !== 'right' || this.game.parts.length === 0)
+      )
+        this.game.curDir = 'left';
+      else if (
+        e.key === 'ArrowUp' &&
+        (this.game.curDir !== 'down' || this.game.parts.length === 0)
+      )
+        this.game.curDir = 'up';
+      else if (
+        e.key === 'ArrowDown' &&
+        (this.game.curDir !== 'up' || this.game.parts.length === 0)
+      )
+        this.game.curDir = 'down';
     });
   }
 }
 
-class Area {
+class Food {
   constructor(game) {
     this.game = game;
-    this.squares = [];
-  }
-
-  draw(context) {
-    //console.log(this.squares);
-    if (this.squares.length) {
-      this.squares.forEach((square) => {
-        context.drawImage(
-          square.image,
-          square.x,
-          square.y,
-          this.game.squareWidth,
-          this.game.squareHeight
-        );
-      });
-    }
-  }
-}
-
-class Piece {
-  constructor(game, type) {
-    this.game = game;
-    this.x = 0;
-    this.y = 0;
-    this.matrix = [];
-    this.squares = [];
-    this.moving = true;
+    this.x = Math.floor((Math.random() * canvas.width) / 10) * 10;
+    this.y = Math.floor((Math.random() * canvas.height) / 10) * 10;
+    this.width = 10;
+    this.height = 10;
     this.image = new Image();
-
-    switch (type) {
-      case 0:
-        this.image.src = './assets/yellow-block.png';
-        this.matrix = [
-          [0, 0, 0, 0],
-          [1, 1, 1, 1],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0],
-        ];
-        break;
-      case 1:
-        this.image.src = './assets/light-orange-block.png';
-        this.matrix = [
-          [0, 0, 1],
-          [1, 1, 1],
-          [0, 0, 0],
-        ];
-        break;
-      case 2:
-        this.image.src = './assets/light-orange-block.png';
-        this.matrix = [
-          [1, 0, 0],
-          [1, 1, 1],
-          [0, 0, 0],
-        ];
-        break;
-      case 3:
-        this.image.src = './assets/green-block.png';
-        this.matrix = [
-          [0, 0, 0],
-          [1, 1, 1],
-          [0, 1, 0],
-        ];
-        break;
-      case 4:
-        this.image.src = './assets/orange-block.png';
-        this.matrix = [
-          [0, 1, 1],
-          [1, 1, 0],
-          [0, 0, 0],
-        ];
-        break;
-      case 5:
-        this.image.src = './assets/orange-block.png';
-        this.matrix = [
-          [1, 1, 0],
-          [0, 1, 1],
-          [0, 0, 0],
-        ];
-        break;
-      case 6:
-        this.image.src = './assets/red-block.png';
-        this.matrix = [
-          [1, 1],
-          [1, 1],
-        ];
-        break;
-    }
   }
 
   update() {
-    if (this.game.isColliding(this.squares)) {
-      this.game.handlePieceCollision(this.squares, this.image);
-    } else this.y += this.game.squareHeight;
+    this.checkCollision();
   }
 
   draw(context) {
-    this.squares = [];
-
-    for (var i = 0; i < this.matrix.length; i++) {
-      for (var j = 0; j < this.matrix[i].length; j++) {
-        if (this.matrix[i][j] === 1) {
-          this.squares.push({
-            x: this.x + j * this.game.squareWidth,
-            y: this.y + i * this.game.squareHeight,
-          });
-        }
-      }
-    }
-
-    this.squares.forEach((square) => {
-      // context.fillStyle = 'purple';
-      // context.fillRect(
-      //   square.x,
-      //   square.y,
-      //   this.game.squareWidth,
-      //   this.game.squareHeight
-      // );
-
-      context.drawImage(
-        this.image,
-        square.x,
-        square.y,
-        this.game.squareWidth,
-        this.game.squareHeight
-      );
-    });
+    //context.fillStyle = 'red';
+    //context.fillRect(this.x, this.y, this.width, this.height);
+    this.image.src = './assets/food.png';
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 
-  matrixToRight() {
-    let auxMatrix = [];
-    let auxVet = [];
-    let auxI = 0;
-    let auxJ = 0;
+  checkCollision() {
+    if (
+      this.game.firstPart.x + this.game.firstPart.width > this.x &&
+      this.game.firstPart.x < this.x + this.width &&
+      this.game.firstPart.y + this.game.firstPart.height > this.y &&
+      this.game.firstPart.y < this.y + this.height
+    ) {
+      this.game.eatFood();
+    }
+  }
+}
 
-    for (var j = 0; j < this.matrix.length; j++) {
-      for (var i = this.matrix.length - 1; i >= 0; i--) {
-        //console.log('Sexo ->' + this.matrix[i][j]);
-        auxVet.push(this.matrix[i][j]);
-        //auxMatrix[auxI][auxJ] = this.matrix[i][j];
-        auxI += 1;
-      }
-      auxMatrix.push(auxVet);
-      auxVet = [];
-      auxJ += 1;
+class Part {
+  constructor(game, x, y, moves, image) {
+    this.game = game;
+    this.width = 10;
+    this.height = 10;
+    this.x = x;
+    this.y = y;
+    this.moves = moves;
+    this.image = image;
+  }
+
+  draw(context) {
+    //context.fillStyle = 'white';
+    //context.fillRect(this.x, this.y, this.width, this.height);
+
+    context.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+
+  update() {
+    switch (this.moves[0]) {
+      case 'right':
+        this.x += this.game.speed;
+        break;
+      case 'down':
+        this.y += this.game.speed;
+        break;
+      case 'left':
+        this.x -= this.game.speed;
+        break;
+      case 'up':
+        this.y -= this.game.speed;
+        break;
     }
 
-    this.matrix = auxMatrix;
-    //console.log(this.matrix);
+    this.moves.splice(0, 1);
+    this.moves.push(this.game.curDir);
   }
 }
 
 class Game {
   constructor(width, height) {
-    this.first = true;
     this.width = width;
     this.height = height;
-    this.squareWidth = 30;
-    this.squareHeight = 30;
-    this.nextPiece = null;
-    this.currentPiece = null;
+    this.points = 0;
+    this.curDir = 'right';
+    this.speed = 10;
+    this.parts = [];
+    this.firstPart = new Part(this, 0, 0, [this.curDir]);
+    this.food = new Food(this);
     this.inputHandler = new InputHandler(this);
-    this.area = new Area(this);
-    this.pieceTimer = 0;
-    this.pieceInterval = 350;
-    this.score = 0;
-    this.scoreValue = 50;
-    this.gameTimer = 0;
-    this.gameInterval = 1000;
-    this.gameTime = 0;
-
-    // Sort first and next piece for starting the game
-    const randPiece = Math.floor(Math.random() * 7);
-    const randNextPiece = Math.floor(Math.random() * 7);
-
-    this.currentPiece = new Piece(this, randPiece);
-    this.nextPiece = new Piece(this, randNextPiece);
+    this.moveTimer = 0;
+    this.moveInterval = 50;
   }
 
   update(deltaTime) {
-    if (this.pieceTimer < this.pieceInterval) {
-      this.pieceTimer += deltaTime;
+    if (this.moveTimer < this.moveInterval) {
+      this.moveTimer += deltaTime;
     } else {
-      this.currentPiece.update();
-      this.pieceTimer = 0;
-    }
+      this.firstPart.update();
+      this.parts.forEach((part) => {
+        part.update();
+      });
 
-    if (this.gameTimer < this.gameInterval) {
-      this.gameTimer += deltaTime;
-    } else {
-      this.gameTime += 1;
-      this.gameTimer = 0;
-    }
+      if (this.checkPartsCollision()) {
+        if (confirm('Você perdeu! Deseja continuar?')) {
+          startGame();
+        }
+      }
 
-    this.renderUI();
+      this.food.update();
+
+      this.moveTimer = 0;
+    }
   }
 
   draw(context) {
-    this.currentPiece.draw(context);
+    this.food.draw(context);
 
-    this.area.draw(context);
-  }
+    let headImage = new Image();
+    let image = new Image();
 
-  generatePiece() {
-    this.currentPiece = this.nextPiece;
-    this.nextPiece = new Piece(this, Math.floor(Math.random() * 7));
+    // HANDLE HEAD IMAGE
+    switch (this.curDir) {
+      case 'right':
+        headImage.src = './assets/head-right.png';
+        break;
+      case 'left':
+        headImage.src = './assets/head-left.png';
+        break;
+      case 'up':
+        headImage.src = './assets/head-up.png';
+        break;
+      case 'down':
+        headImage.src = './assets/head-down.png';
+        break;
+    }
 
-    // Verify if you lose
-    var auxSquares = [];
+    this.firstPart.image = headImage;
+    this.firstPart.draw(context);
 
-    for (var i = 0; i < this.currentPiece.matrix.length; i++) {
-      for (var j = 0; j < this.currentPiece.matrix[i].length; j++) {
-        if (this.currentPiece.matrix[i][j] === 1) {
-          auxSquares.push({
-            x: this.currentPiece.x + j * this.squareWidth,
-            y: this.currentPiece.y + i * this.squareHeight,
-          });
+    if (this.parts.length > 0) {
+      var prevPart;
+      var nextPart;
+
+      this.parts.forEach((part, index) => {
+        if (this.parts.length === 1) {
+          prevPart = this.firstPart;
+          nextPart = null;
+        } else {
+          if (index === 0) {
+            prevPart = this.firstPart;
+          } else prevPart = this.parts[index - 1];
+
+          if (index === this.parts.length - 1) {
+            nextPart = null;
+          } else {
+            nextPart = this.parts[index + 1];
+          }
         }
-      }
-    }
 
-    if (this.isColliding(auxSquares)) {
-      alert('Você perdeu, tentar novamente!');
-      this.restartGame();
-    }
-  }
+        //console.log(nextPart);
+        let auxArr = this.handleNeighbors(part.x, part.y, prevPart, nextPart);
 
-  restartGame() {
-    startGame();
-  }
-
-  isColliding(squares) {
-    var res = false;
-
-    // Check collision with the bottom
-    squares.forEach((square) => {
-      if (square.y + this.squareHeight >= this.height && res === false) {
-        res = true;
-      }
-    });
-
-    // Check collision with other parts of the area
-    for (var i = 0; i < this.area.squares.length; i++) {
-      for (var j = 0; j < squares.length; j++) {
-        if (
-          squares[j].y + this.squareHeight === this.area.squares[i].y &&
-          squares[j].x === this.area.squares[i].x &&
-          res === false
-        ) {
-          //console.log('Square Bottom -> ' + (squares[j].y + this.squareHeight));
-          //console.log('Area Square Top -> ' + this.area.squares[i].y);
-          res = true;
+        if (auxArr.length === 1) {
+          if (auxArr[0] === 'up')
+            this.parts[index].image.src = './assets/tale-bottom.png';
+          else if (auxArr[0] === 'down')
+            this.parts[index].image.src = './assets/tale-top.png';
+          else if (auxArr[0] === 'left')
+            this.parts[index].image.src = './assets/tale-right.png';
+          else if (auxArr[0] === 'right')
+            this.parts[index].image.src = './assets/tale-left.png';
+        } else {
+          if (auxArr.includes('up') && auxArr.includes('down'))
+            this.parts[index].image.src = './assets/vertical-body.png';
+          else if (auxArr.includes('left') && auxArr.includes('right'))
+            this.parts[index].image.src = './assets/horizontal-body.png';
+          else if (auxArr.includes('right') && auxArr.includes('down'))
+            this.parts[index].image.src = './assets/border1.png';
+          else if (auxArr.includes('down') && auxArr.includes('left'))
+            this.parts[index].image.src = './assets/border2.png';
+          else if (auxArr.includes('left') && auxArr.includes('up'))
+            this.parts[index].image.src = './assets/border3.png';
+          else if (auxArr.includes('up') && auxArr.includes('right'))
+            this.parts[index].image.src = './assets/border4.png';
         }
-      }
-    }
 
-    return res;
-  }
-
-  isFree(direction) {
-    var response = true;
-
-    this.currentPiece.squares.forEach((square) => {
-      // verify game borders
-      if (square.x === 0 && direction === 'left') response = false;
-      else if (
-        square.x + this.squareWidth === this.width &&
-        direction === 'right'
-      )
-        response = false;
-      else if (
-        square.y + this.squareHeight === this.height &&
-        direction === 'down'
-      ) {
-        response = false;
-      }
-
-      // Verify other area pieces
-      this.area.squares.forEach((areaSquare) => {
-        if (
-          square.x === areaSquare.x + this.squareWidth &&
-          square.y === areaSquare.y &&
-          direction === 'left'
-        )
-          response = false;
-        else if (
-          square.x + this.squareWidth === areaSquare.x &&
-          square.y === areaSquare.y &&
-          direction === 'right'
-        )
-          response = false;
-        else if (
-          square.y + this.squareHeight === areaSquare.y &&
-          square.x === areaSquare.x &&
-          direction === 'down'
-        )
-          response = false;
+        part.draw(context);
       });
-    });
+    }
+  }
+
+  eatFood() {
+    this.food = new Food(this);
+    this.points += 10;
+
+    let moves = [this.firstPart.moves[0]];
+    let yOffset;
+    let xOffset;
+
+    switch (this.firstPart.moves[0]) {
+      case 'up':
+        yOffset = 10;
+        xOffset = 0;
+        break;
+      case 'down':
+        yOffset = -10;
+        xOffset = 0;
+        break;
+      case 'left':
+        xOffset = 10;
+        yOffset = 0;
+        break;
+      case 'right':
+        xOffset = -10;
+        yOffset = 0;
+        break;
+    }
+
+    if (this.parts.length) {
+      this.parts[this.parts.length - 1].moves.forEach((move) => {
+        moves.push(move);
+      });
+    } else {
+      this.firstPart.moves.forEach((move) => {
+        moves.push(move);
+      });
+    }
+
+    if (this.parts.length) {
+      this.parts.push(
+        new Part(
+          this,
+          this.parts[this.parts.length - 1].x + xOffset,
+          this.parts[this.parts.length - 1].y + yOffset,
+          moves,
+          new Image()
+        )
+      );
+    } else {
+      this.parts.push(
+        new Part(
+          this,
+          this.firstPart.x + xOffset,
+          this.firstPart.y + yOffset,
+          moves,
+          new Image()
+        )
+      );
+    }
+  }
+
+  checkPartsCollision() {
+    var response = false;
+
+    // Check collision with other parts
+    if (this.parts.length) {
+      this.parts.forEach((part) => {
+        if (
+          part.x + part.width > this.firstPart.x &&
+          this.curDir !== 'left' &&
+          part.x < this.firstPart.x + this.firstPart.width &&
+          part.y + part.height > this.firstPart.y &&
+          part.y < this.firstPart.y + this.firstPart.height &&
+          response === false
+        )
+          response = true;
+      });
+    }
+
+    // Check collision with borders
+    if (
+      this.firstPart.x < 0 ||
+      this.firstPart.x + this.firstPart.width > this.width ||
+      this.firstPart.y < 0 ||
+      this.firstPart.y + this.firstPart.height > this.height
+    )
+      response = true;
 
     return response;
   }
 
-  handlePieceCollision(squares, image) {
-    squares.forEach((square, index) => {
-      this.area.squares.push({
-        x: square.x,
-        y: square.y,
-        image: image,
-      });
-    });
-    this.checkLinesAndEliminate();
-    this.generatePiece();
-  }
+  handleNeighbors(x, y, prevPart, nextPart) {
+    let auxArr = [];
 
-  checkLinesAndEliminate() {
-    var lineCount = 0;
+    //console.log(prevPart);
 
-    for (var i = 0; i < this.height / this.squareHeight; i++) {
-      var lineCount = 0;
-      for (var j = 0; j < this.width / this.squareWidth; j++) {
-        //console.log('Y -> ' + i * this.squareHeight);
-        //console.log('X -> ' + j * this.squareWidth);
-        this.area.squares.forEach((square) => {
-          if (
-            square.x === j * this.squareWidth &&
-            square.y === i * this.squareHeight
-          ) {
-            lineCount += 1;
-          }
-        });
-      }
+    // Handle Prev Part
+    if (x === prevPart.x + prevPart.width) auxArr.push('left');
+    else if (x === prevPart.x - prevPart.width) auxArr.push('right');
+    else if (y === prevPart.y + prevPart.height) auxArr.push('up');
+    else if (y === prevPart.y - prevPart.height) auxArr.push('down');
 
-      //console.log('Quadrado na linha ' + i + ' -> ' + lineCount);
-
-      if (lineCount === this.width / this.squareWidth) {
-        var newArea = this.area.squares.filter(
-          (item) => item.y !== i * this.squareHeight
-        );
-        this.area.squares = newArea;
-
-        this.area.squares.map((item) => {
-          if (item.y < i * this.squareHeight) {
-            console.log('descer');
-            item.y += this.squareHeight;
-          }
-        });
-
-        this.score += this.scoreValue;
-      }
+    // // Handle Next Part
+    if (nextPart !== null) {
+      if (x === nextPart.x + nextPart.width) auxArr.push('left');
+      else if (x === nextPart.x - nextPart.width) auxArr.push('right');
+      else if (y === nextPart.y + nextPart.height) auxArr.push('up');
+      else if (y === nextPart.y + nextPart.height) auxArr.push('down');
     }
-  }
 
-  renderUI() {
-    const scoreEl = document.getElementById('score');
-    const timerEl = document.getElementById('timer');
-    const nextPieceEl = document.getElementById('nextPiece');
-    scoreEl.innerHTML = '';
-    timerEl.innerHTML = '';
-    nextPieceEl.innerHTML = '';
-
-    scoreEl.appendChild(document.createTextNode(this.score));
-
-    let min = Math.floor(this.gameTime / 60);
-    if (min < 10) min = '0' + min;
-    let sec = this.gameTime % 60;
-    if (sec < 10) sec = '0' + sec;
-    timerEl.appendChild(document.createTextNode(min + ':' + sec));
+    return auxArr;
   }
 }
 
@@ -416,9 +339,9 @@ startGame();
 
 let lastTime = 0;
 
-function animate(timeStamps) {
-  let deltaTime = timeStamps - lastTime;
-  lastTime = timeStamps;
+function animate(timestamps) {
+  let deltaTime = timestamps - lastTime;
+  lastTime = timestamps;
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
